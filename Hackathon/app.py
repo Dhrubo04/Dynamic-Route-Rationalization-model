d73cfde86019a325e32443eb9e3851815b81546f
Dhrubo <132836713+Dhrubo04@users.noreply.github.com>
2024-09-04 15:58:00 +0530

    Update app.py

--- Hackathon/app.py
+++ Hackathon/app.py
@@ -1,14 +1,19 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, request, jsonify
+import json
 
-app = Flask(__name__, static_folder='static')
+app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/static/<path:filename>')
-def serve_static(filename):
-    return send_from_directory('static', filename)
+@app.route('/api/get_data', methods=['GET'])
+def get_data():
+    # Example endpoint to return data if needed by your JavaScript
+    data = {
+        "example": "data"
+    }
+    return jsonify(data)
 
 if __name__ == '__main__':
     app.run(debug=True)
