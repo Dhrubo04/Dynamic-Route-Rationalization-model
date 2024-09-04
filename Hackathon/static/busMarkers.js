d46d4a30f2c7c639eed87103e2ddf4487e9da367
Dhrubo <132836713+Dhrubo04@users.noreply.github.com>
2024-09-05 03:24:26 +0530

    Bus Coordinates

---
+++ Hackathon/static/busMarkers.js
@@ -0,0 +1,7 @@
+const busMarkersData=[
+    { lat: 22.5151, lng: 88.3931 }, // Acropolis Mall
+    { lat: 22.5142, lng: 88.3981 }, // GST Bhavan
+    { lat: 22.5198, lng: 88.3821 }, // KASBA
+    { lat: 22.5536, lng: 88.3492 }, // Park Street
+    { lat: 22.4666, lng: 88.3604 }  // Garia
+];
