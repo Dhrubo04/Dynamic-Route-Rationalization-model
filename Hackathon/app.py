d341261a0d99e3ecb9f9a794b3a3f704428e9c58
Dhrubo <132836713+Dhrubo04@users.noreply.github.com>
2024-09-05 17:19:52 +0530

    Update app.py
    with ML model  Loaded

--- Hackathon/app.py
+++ Hackathon/app.py
@@ -1,19 +1,35 @@
 from flask import Flask, render_template, request, jsonify
-import json
+import joblib
+import numpy as np
 
 app = Flask(__name__)
+
+# Loading
+model = joblib.load('traffic_duration_model.pkl')
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/api/get_data', methods=['GET'])
-def get_data():
-    # Example endpoint to return data if needed by your JavaScript
-    data = {
-        "example": "data"
-    }
-    return jsonify(data)
+@app.route('/api/get_predicted_duration', methods=['POST'])
+def get_predicted_duration():
+    try:
+        data = request.json
+        features = np.array([[
+            data['hour'], 
+            data['day_of_week'], 
+            data['distance'], 
+            data['duration_in_traffic']
+        ]])
+        
+        # Predict using the model
+        predicted_duration = model.predict(features)
+        
+        # Return the prediction as JSON
+        return jsonify(predicted_duration=predicted_duration[0])
+    
+    except Exception as e:
+        return jsonify(error=str(e)), 400
 
 if __name__ == '__main__':
     app.run(debug=True)
